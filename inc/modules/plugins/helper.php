5d7509cf05c17df66ec8172b31bd06d73b0cebf7
Alireza <alirezadarvyshy@gmail.com>
2024-03-19 05:14:40 -0400

    remove tgm plugin activation

--- inc/modules/plugins/helper.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) exit;
-
-if ( is_admin() ) {
-	require_once LIAN_MODULE_DIR . '/plugins/class-tgm-plugin-activation.php';
-	require_once LIAN_MODULE_DIR . '/plugins/tgm-plugin-activation.php';
-}
