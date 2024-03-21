6dc8e65aebc051d04abcdc4965b6c5f0fa23efdd
Alireza <alirezadarvyshy@gmail.com>
2024-03-21 07:33:34 -0400

    add sections for style options

--- inc/options/settings/frontend/options-style.php
+++ inc/options/settings/frontend/options-style.php
@@ -7,7 +7,7 @@
 ?>
 <li>
   <div>
-    <h2><?php _e( 'Colors', 'lian' ); ?></h2>
+    <h2><?php _e( 'General', 'lian' ); ?></h2>
     <div class="row">
       <div class="col-4"><?php _e( 'Main Color', 'lian' ); ?></div>
       <div class="col">
@@ -50,6 +50,22 @@
       </div>
     </div>
 
+    <h2><?php _e( 'Links', 'lian' ); ?></h2>
+    
+    <div class="row">
+      <div class="col-4"><?php _e( 'Link Text Decoration', 'lian' ); ?></div>
+      <div class="col">
+        <select name="lian_options[lian_style_link_textdeco]">
+          <option value=""><?php _e( '-- Choose --', 'lian' ); ?></option>
+          <option value="none" <?php echo selected('none', self::get_theme_option( 'lian_style_link_textdeco' ), true); ?>><?php _e( 'None', 'lian' ); ?></option>
+          <option value="underline" <?php echo selected('underline', self::get_theme_option( 'lian_style_link_textdeco' ), true); ?>><?php _e( 'Underline', 'lian' ); ?></option>
+          <option value="overline" <?php echo selected('overline', self::get_theme_option( 'lian_style_link_textdeco' ), true); ?>><?php _e( 'Overline', 'lian' ); ?></option>
+          <option value="line-through" <?php echo selected('line-through', self::get_theme_option( 'lian_style_link_textdeco' ), true); ?>><?php _e( 'Line Through', 'lian' ); ?></option>
+        </select>
+
+      </div>
+    </div>
+    
     <div class="row">
       <div class="col-4"><?php _e( 'Link Color', 'lian' ); ?></div>
       <div class="col">
@@ -63,6 +79,8 @@
         <input name="lian_options[lian_style_a_color_hover]" type="text" value="<?php echo self::get_theme_option( 'lian_style_a_color_hover' ); ?>" class="lian-color" data-default-color="#555555" />
       </div>
     </div>
+
+    <h2><?php _e( 'Buttons', 'lian' ); ?></h2>
 
     <div class="row">
       <div class="col-4"><?php _e( 'Button Background Color', 'lian' ); ?></div>
@@ -91,13 +109,8 @@
         <input name="lian_options[lian_style_button_color_hover]" type="text" value="<?php echo self::get_theme_option( 'lian_style_button_color_hover' ); ?>" class="lian-color" data-default-color="#555555" />
       </div>
     </div>
-
-    <div class="row">
-      <div class="col-4"><?php _e( 'Price Color', 'lian' ); ?></div>
-      <div class="col">
-        <input name="lian_options[lian_style_price_color]" type="text" value="<?php echo self::get_theme_option( 'lian_style_price_color' ); ?>" class="lian-color" data-default-color="#555555" />
-      </div>
-    </div>
+    
+    <h2><?php _e( 'Inputs', 'lian' ); ?></h2>
 
     <div class="row">
       <div class="col-4"><?php _e( 'Input Text Color', 'lian' ); ?></div>
@@ -127,6 +140,8 @@
       </div>
     </div>
 
+    <h2><?php _e( 'Breadcrumb', 'lian' ); ?></h2>
+
     <div class="row">
       <div class="col-4"><?php _e( 'Breadcrumb Text Color', 'lian' ); ?></div>
       <div class="col">
@@ -134,5 +149,14 @@
       </div>
     </div>
 
+    <h2><?php _e( 'WooCommerce', 'lian' ); ?></h2>
+
+    <div class="row">
+      <div class="col-4"><?php _e( 'Price Color', 'lian' ); ?></div>
+      <div class="col">
+        <input name="lian_options[lian_style_price_color]" type="text" value="<?php echo self::get_theme_option( 'lian_style_price_color' ); ?>" class="lian-color" data-default-color="#555555" />
+      </div>
+    </div>
+    
   </div>
 </li>
